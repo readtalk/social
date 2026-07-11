d3df532f3fc3e2aad6d86ce32b15a726e1949930
READTalk <redtalk.app@hotmail.com>
2026-07-11 20:46:00 +0700

    Rename settings.ts to settings.tsx

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -2,7 +2,7 @@ export function SettingsHTML(user: { id: string; email: string }) {
   return `<!DOCTYPE html>
 <html>
 <head>
-  <title>JWT</title>
+  <title>Home</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <script src="https://cdn.tailwindcss.com"></script>
