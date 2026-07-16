a820d1f8bf38154e31acd12ea188b07b6ae4039a
READTalk <redtalk.app@hotmail.com>
2026-07-16 21:29:36 +0700

    Change title from 'Profile' to 'Homepage'

--- app/routes/Home.tsx
+++ app/routes/Home.tsx
@@ -1,8 +1,8 @@
-export function SettingsHTML(user: { id: string; email: string }) {
+export function HomeHTML(user: { id: string; email: string }) {
   return `<!DOCTYPE html>
 <html>
 <head>
-  <title>Profile</title>
+  <title>Homepage</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />  
   
