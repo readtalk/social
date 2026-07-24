c63fbbd94b2bb16e31eda209dc5e07a05f08f02b
READTalk <redtalk.app@hotmail.com>
2026-07-24 23:39:25 +0700

    Update and rename auth.tsx to auth.tsx

--- src/auth.tsx
+++ src/auth.tsx
@@ -4,7 +4,7 @@ import { PasswordProvider } from "@openauthjs/openauth/provider/password";
 import { PasswordUI } from "@openauthjs/openauth/ui/password";
 import { createSubjects } from "@openauthjs/openauth/subject";
 import { object, string } from "valibot";
-import { HomeHTML } from "../public/home";
+import { HomeHTML } from "../src/home";
 
 const subjects = createSubjects({
   user: object({
