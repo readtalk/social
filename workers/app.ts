491c95386e51f93261a99c12832c9074b483dec4
READTalk <redtalk.app@hotmail.com>
2026-07-28 06:31:36 +0700

    Remove comment for auth.ts in app.ts

--- workers/app.ts
+++ workers/app.ts
@@ -1,4 +1,3 @@
-// workers/auth.ts
 import { issuer } from "@openauthjs/openauth";
 import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare";
 import { PasswordProvider } from "@openauthjs/openauth/provider/password";
